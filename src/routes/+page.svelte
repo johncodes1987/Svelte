8393137bd8c084d9efdc185ed26be9ed44a3fa4e
John Nance <119992337+johncodes1987@users.noreply.github.com>
2025-10-29 14:24:07 -0500

    Second

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang='ts'>
+    import Header from "./Header.svelte";
+    let name = $state('John');
+
+    let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+
+    // function toggle() {
+    //     status = status === 'OPEN'? 'CLOSED' : 'OPEN';
+    // }
+    // Same as below, but using a function to toggle the status instead of a ternary operator.
+
+    function onclick() {
+        status = status === 'OPEN'? 'CLOSED' : 'OPEN';
+    }
+
+</script>
+
+<Header name={name}/>
+
+<input type='text' bind:value={name} />
+
+<p>The store is now {status}!</p>
+<button {onclick}>Toggle Status</button>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+    let {
+        name
+        } : {
+            name: string;
+        } = $props();
+</script>
+
+<h1>Hello {name}</h1>
+<h2>{name.replace('o', 'x')}</h2>
+
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
+
